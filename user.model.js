08feead3d1a3338065b5ba78dd6316f8f075cf23
Parth Nandaniya <parthnandaniya@gmail.com>
2019-06-27 11:37:08 +0530

    backend complete

---
+++ user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let User = new Schema({
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String
+    },
+    date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('User', User);
